f65581684bc328cbb21f3f0f43257eb35a709322
Bill Leonhardt <iamleonhardt@gmail.com>
2017-10-01 01:48:22 -0700

    cleanup

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var num2 = '';
 var op = '';
 var result = '';
 
-
-// DOC READY - 4 FUNCTIONS - .NUMBERS, .OPERATORS, .SPECIAL, .ANSWER BUTTONS
 $(document).ready(applyClicks);
 
 function applyClicks() {
@@ -16,14 +14,10 @@ function applyClicks() {
     $('.numButton').click(function () {
         addDigits($(this).text());
     });
-    // KEYBOARD INPUT TO BUTTONS
-    $('body').keypress(keyboardPressed);
-    
     // DECIMAL #decimal
     $('.decimal').click(function () {
         addDec($(this).text());
     });
-
     // OPERATORS .arithButton
     $('.arithButton').click(function () {
         addOps($(this).text());
@@ -39,18 +33,16 @@ function applyClicks() {
     // ANSWER #equal
     $('#equal').click(function () {
         addAnswer();
-    });
-};
+    })
+    // KEYBOARD INPUT TO BUTTONS
+    $('body').keypress(keyboardPressed);
+
+}
 
 // Function for adding keyboard press to buttons
 function keyboardPressed(event){
     var eWhich = event.which;
-    console.log('the event.which is : ', event.which);
     var character = String.fromCharCode(eWhich);
-    console.log('character is : ', character);
-    // if(event.which === 13){
-    // character = '=';
-    // }
 
     switch (event.which){
         case 13:
@@ -66,41 +58,35 @@ function keyboardPressed(event){
     element.click();
 }
 
-
 // FUNCTION FOR ADDING DIGIT WHEN NUMBERS ARE PRESSED
 // Take in number and add it to the array. Should be concat to other numbers previously pressed in the 0 position
 function addDigits(inputDigit) {
     inputArray[arrayPos] += inputDigit;
-    console.log(inputArray);
-    // SHOW IN DISPLAY
     displayResult();
+    console.log(inputArray);
 }
-
 
 // FUNCTION FOR ADDING OPERATORS
 // increase arrayPos, set value, increase position, clear position
 function addOps(inputOps) {
+    // if an operator has already been pushed remove it and use new one
     if (arrayPos > 0 && inputArray[arrayPos] == '') {
-        console.log("checking if inputArray[arrayPos] is empty string " + (inputArray[arrayPos] == ''));
         inputArray.pop();
-        console.log(inputArray);
-        console.log("Array Position is : " + arrayPos);
         arrayPos--;
-        console.log("Array Position is : " + arrayPos);
         inputArray[arrayPos] = inputOps;
         displayResult();
         arrayPos++;
         inputArray[arrayPos] = '';
+        console.log(inputArray);
     }
+    // if at first position and no numbers pushed, return
     else if (inputArray[arrayPos] == '') {
         console.log(inputArray);
         return;
     }
-    else
-    {
+    else {
         arrayPos++;
         inputArray[arrayPos] = inputOps;
-        // SHOW IN DISPLAY
         displayResult();
         arrayPos++;
         inputArray[arrayPos] = '';
@@ -108,30 +94,24 @@ function addOps(inputOps) {
     }
 }
 
-
-//FUNCTION FOR ADDING DECIMALS
 function addDec(inputDigit) {
     var posIndexString = inputArray[arrayPos];
-    console.log(posIndexString);
     if (posIndexString.indexOf(".") == -1) {
         inputArray[arrayPos] += inputDigit;
-        console.log(inputArray);
         displayResult();
+        console.log(inputArray);
     }
 }
 
-
-// FUNCTION FOR ADDING CLEARS
-// Clear
 function addC() {
     inputArray = [''];
     arrayPos = 0;
-    console.log(inputArray);
     displayResult();
+    console.log(inputArray);
 }
-// Clear entry
+
 function addCE() {
-    //two separate things depending on the condition. condition: if ''
+    //two separate things depending on the condition: if ''
     //if '' remove string and previous array location
     //if not set array pos to ''
     if (arrayPos == 0 && inputArray[arrayPos] == '') {
@@ -153,31 +133,22 @@ function addCE() {
     else {
         inputArray[arrayPos] = '';
         displayResult();
-        // inpArrayPos = inputArray[arrayPos];
-        // inpArrayPos = inpArrayPos.slice(0, inpArrayPos.length-1);
     }
     console.log(inputArray);
 }
 
-// FUNCTION FOR ANSWER BUTTON
 function assignNums() {
     num1 = parseFloat(inputArray[0]);
     num2 = parseFloat(inputArray[2]);
     op = inputArray [1];
     console.log("num1 = " + num1 + ", num2 = " + num2 + ", op = " + op);
-};
-
+}
 
 function addAnswer() {
-    // var hold = "";
     // Check for Number and Operator
     if (inputArray.length == 1 && inputArray[0] == '') {
         $('.screen').text("0");
     }
-    // Trying to use var hold to add operator when you hit =
-    // if (inputArray.length == 1 && inputArray[0] != NaN) {
-    //
-    // }
     while (inputArray.length >= 2) {
         // Check if Two numbers and Operator
         if (inputArray.length >= 3 && inputArray[inputArray.length - 1] != '') {
@@ -188,26 +159,13 @@ function addAnswer() {
             assignNums();
         }
         doMath();
-        // if (inputArray.length == 3){
-        //     inputArray[0] = result;
-        //     console.log("inputArray[0] = " + inputArray[0]);
-        //     console.log("inputArray = " + inputArray);
-        //     hold = inputArray[1] + inputArray[2];
-        //     inputArray.splice(2,1);
-        //     arrayPos -= 1;
-        //     console.log("inputArray = " + inputArray);
-        // }
-        // else {
         inputArray[0] = result;
-        console.log("inputArray[0] = " + inputArray[0]);
-        console.log("inputArray = " + inputArray);
         inputArray.splice(1, 2);
         arrayPos -= 2;
         console.log("inputArray = " + inputArray);
     }
-};
+}
 
-// DO MATH
 function doMath() {
     switch (op) {
         case "+":
@@ -233,7 +191,6 @@ function doMath() {
     $('.screen').text(result);
 }
 
-// FUNCTION FOR SHOWING DISPLAY
 function displayResult() {
     $('.screen').text(inputArray[arrayPos]);
 }
